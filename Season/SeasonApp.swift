e45253804356afb82159b1cead563ef57643c758
Nova1390 <roccodaffuso@gmail.com>
2026-03-17 11:09:36 +0100

    Initial Commit

---
+++ Season/SeasonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonApp.swift
+//  Season
+//
+//  Created by Rocco D'Affuso on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SeasonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
